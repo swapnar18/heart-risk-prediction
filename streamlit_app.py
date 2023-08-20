51a4896b798758c32ab15428cab727d81dfac035
swapnar18 <141568877+swapnar18@users.noreply.github.com>
2023-08-20 10:16:27 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,19 @@
 import streamlit as st
 
+# title_alignment=
+# """
+# <style>
+# #the-title {
+#   text-align: center
+# }
+# </style>
+# """
+# st.markdown(title_alignment, unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: center; color: red;'>Welcome to Heart Risk Prediction App Using Machine Learning</h1>", unsafe_allow_html=True)
+
 # st.write('Hello world!')
-st.title("Welcome to Heart Risk Prediction App Using Machine Learning", anchor=None, *, help=None)
+# st.title("Welcome to Heart Risk Prediction App Using Machine Learning", anchor=None, *, help=None)
 
 with st.form(key='columns_in_form'):
     cols = st.columns(5)
