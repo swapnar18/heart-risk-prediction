705e778ce55f528d2aa7603ee4648fa8ca23d089
swapnar18 <141568877+swapnar18@users.noreply.github.com>
2023-08-20 10:57:02 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,20 +20,32 @@ st.caption('Please enter your health details below to get your personalized hear
 # st.caption('A caption with _italics_ :blue[colors] and emojis :heartpulse:')
 columns = ['Gender', 'age', 'education', 'currentSmoker', 'cigsPerDay', 'BPMeds','prevalentStroke', 'prevalentHyp', 'diabetes', 'totChol', 'sysBP',\
            'diaBP', 'BMI', 'heartRate', 'glucose']
-single_select_cols= ['Gender','Education','Habitual Smoking' ,'BP Medication' , 'Any Prevalent Stroke','Any Prevalent Hypertension']
+single_select_cols= ['Gender','Education','Habitual Smoking' ,'BP Medication' , 'Any Prevalent Stroke','Any Prevalent Hypertension','Any Diabetes']
 single_select_dict = {'Gender' :['Male','Female','Others'],
                       'Education' : ['Primary','Secondary','Graduate','Post Grad','Others'],
                       'Habitual Smoking' : ['Yes','No'],
                       'BP Medication' : ['Yes','No'],
                       'Any Prevalent Stroke' : ['Yes','No'],
-                      'Any Prevalent Hypertension' : ['Yes','No']}
+                      'Any Prevalent Hypertension' : ['Yes','No'],
+                      'Any Diabetes' : ['Yes','No']}
 with st.form(key='columns_in_form'):
-    cols = st.columns(6)
-    for i, col in enumerate(cols):
-        col.selectbox(single_select_cols[i], single_select_dict[single_select_cols[i]], key=i)
-    st.slider(label='Select Age', min_value=5, max_value=100, key=6)
-    # st.slider(label='Ciggaretes Per Day (if smoking)', min_value=5, max_value=100, key=6)
-    col1, col2 ,col3 = st.columns(3)
+    col1, col2,col3 = st.columns(3)
     with col1:
-        st.text_input('Ciggaretes Per Day (if smoking)','0',key=7)
+        st.text_input('Enter Your Name','Name',key=1) 
+    with col2:
+        st.text_input('Enter Your Weight in Kgs','1',key=2) 
+    with col3:
+        st.text_input('Enter Your Height in cms','10',key=3) 
+    cols = st.columns(7)
+    for i, col in enumerate(cols):
+        col.selectbox(single_select_cols[i], single_select_dict[single_select_cols[i]], key=3+i)
+    st.slider(label='Select Age', min_value=5, max_value=100, key=11)
+    # st.slider(label='Ciggaretes Per Day (if smoking)', min_value=5, max_value=100, key=6)
+    col4, col5,col6 = st.columns(3)
+    with col4:
+        st.text_input('Ciggaretes Per Day (if smoking)','0',key=12)
+    with col5:
+        st.text_input('Total Cholestrol','0',key=13)
+    with col6:
+        st.text_input('Heart Rate','0',key=14)
     submitted = st.form_submit_button('Submit')
