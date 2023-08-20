aac92fca670081e1c9ee2446b4646fcff1d8e0ee
swapnar18 <141568877+swapnar18@users.noreply.github.com>
2023-08-20 10:46:13 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,5 +33,7 @@ with st.form(key='columns_in_form'):
         col.selectbox(single_select_cols[i], single_select_dict[single_select_cols[i]], key=i)
     st.slider(label='Select Age', min_value=5, max_value=100, key=6)
     # st.slider(label='Ciggaretes Per Day (if smoking)', min_value=5, max_value=100, key=6)
-    st.text_input('Ciggaretes Per Day (if smoking)','0',key=7)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.text_input('Ciggaretes Per Day (if smoking)','0',key=7)
     submitted = st.form_submit_button('Submit')
