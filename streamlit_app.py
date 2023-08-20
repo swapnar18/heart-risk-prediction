a548eb884f24342ff61ea7ddee094a3a074c021e
swapnar18 <141568877+swapnar18@users.noreply.github.com>
2023-08-20 10:23:35 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,12 +10,14 @@ import streamlit as st
 # """
 # st.markdown(title_alignment, unsafe_allow_html=True)
 
-st.markdown("<h1 style='text-align: center; color:#056608;'>Welcome to Heart Risk Prediction App Using Machine Learning</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color:#056608;'>Welcome to Heart Risk Prediction App Using Machine Learning !!!</h1>", unsafe_allow_html=True)
 
 # st.write('Hello world!')
 # st.title("Welcome to Heart Risk Prediction App Using Machine Learning", anchor=None, *, help=None)
-st.caption('Please enter your health details below to get your personalized heart risk probability')
-st.caption('A caption with _italics_ :blue[colors] and emojis :sunglasses:')
+st.caption(' ')
+st.caption(' ')
+st.caption('Please enter your health details below to get your personalized heart risk probability :heartpulse:')
+# st.caption('A caption with _italics_ :blue[colors] and emojis :heartpulse:')
 
 with st.form(key='columns_in_form'):
     cols = st.columns(5)
