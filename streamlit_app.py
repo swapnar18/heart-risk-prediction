bdfba9c65e7dfc72c094333db5bb7e8e6ddef112
swapnar18 <141568877+swapnar18@users.noreply.github.com>
2023-08-20 10:11:01 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # st.write('Hello world!')
 
 with st.form(key='columns_in_form'):
-    cols = st.beta_columns(5)
+    cols = st.columns(5)
     for i, col in enumerate(cols):
         col.selectbox(f'Make a Selection', ['click', 'or click'], key=i)
     submitted = st.form_submit_button('Submit')
