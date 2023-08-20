168efe1e1281c86558867d37d7ac321ea29941f5
swapnar18 <141568877+swapnar18@users.noreply.github.com>
2023-08-20 10:31:15 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,9 +18,17 @@ st.caption(' ')
 st.caption(' ')
 st.caption('Please enter your health details below to get your personalized heart risk probability :heartpulse:')
 # st.caption('A caption with _italics_ :blue[colors] and emojis :heartpulse:')
-
+columns = ['Gender', 'age', 'education', 'currentSmoker', 'cigsPerDay', 'BPMeds','prevalentStroke', 'prevalentHyp', 'diabetes', 'totChol', 'sysBP',\
+           'diaBP', 'BMI', 'heartRate', 'glucose']
+single_select_cols= ['Gender','Education','Habitual Smoking' ,'BP Medication' , 'Any Prevalent Stroke','Any Prevalent Hypertension']
+single_select_dict = {'Gender' :['Yes','No'],
+                      'Education' : ['Primary','Secondary','Graduate','Post Grad','Others'],
+                      'Habitual Smoking' : ['Yes','No'],
+                      'BP Medication' : ['Yes','No'],
+                      'Any Prevalent Stroke' : ['Yes','No'],
+                      'Any Prevalent Hypertension' : ['Yes','No']}
 with st.form(key='columns_in_form'):
-    cols = st.columns(5)
+    cols = st.columns(6)
     for i, col in enumerate(cols):
-        col.selectbox(f'Make a Selection', ['click', 'or click'], key=i)
+        col.selectbox(single_select_cols[i], single_select_dict[single_select_cols[i]], key=i)
     submitted = st.form_submit_button('Submit')
